79e34cda65cb0d2e20012cbcae2b7ddebd1e507a
Sungwoo Park_computer <sharkclass@naver.com>
2025-03-15 20:00:16 +0900

    feat: Add assets API

--- backend/asset/urls.py
+++ backend/asset/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from django.urls import path, include
+
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"assets", views.AssetViewSet, basename="assets")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
