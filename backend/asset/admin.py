283b4fc505ba69fc41f4c1ffe642149c59c48b94
Sungwoo Park_computer <sharkclass@naver.com>
2025-03-15 19:49:57 +0900

    chore: Migrate models

--- backend/asset/admin.py
+++ backend/asset/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
+@admin.register(models.Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "asset_type", "is_active")
+    list_filter = ("asset_type", "is_active")
