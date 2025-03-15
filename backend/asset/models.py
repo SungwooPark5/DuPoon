b46425fa505bc4155df2f6856683124bfb8e51e0
Sungwoo Park_computer <sharkclass@naver.com>
2025-03-15 19:44:28 +0900

    feat: Add asset (base app)

---
+++ backend/asset/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+from .enums import AssetType, TransactionType
+
+
+# Create your models here.
+class Asset(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    asset_type = models.CharField(
+        max_length=20, choices=[(tag.name, tag.value) for tag in AssetType]
+    )
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return
+
+    def __unicode__(self):
+        return
+
+
+class AssetType(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    is_investable = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
+class AssetBalance(models.Model):
+
+    asset = models.ForeignKey("Asset", on_delete=models.CASCADE)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    ending_balance = models.DecimalField(max_digits=20, decimal_places=2)
+    balance_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.year}-{self.month} {self.asset.name} {self.ending_balance}"
+
+
+class AssetTransaction(models.Model):
+
+    asset = models.ForeignKey("Asset", on_delete=models.CASCADE)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    type = models.CharField(
+        max_length=20, choices=[(tag.name, tag.value) for tag in TransactionType]
+    )
+    transaction_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.year}-{self.month} {self.asset.name} {self.type} {self.amount}"
