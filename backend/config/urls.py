ddf08e92d4b82babc11ea9bbdff2bbd51c2f59ac
Sungwoo Park_computer <sharkclass@naver.com>
2025-05-12 21:11:08 +0900

    refactor: Delete all django app for backend initialization (#1)

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("api/", include("asset.urls")),
 ]
