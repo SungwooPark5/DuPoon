8b96c2d023274ca584e94b8b4591c7a8780aedab
Sungwoo Park_computer <sharkclass@naver.com>
2025-05-12 21:30:55 +0900

    feat: Add core app and create templates folder (#1)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 from environs import Env
@@ -45,7 +47,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     # My Apps
-    
+    "apps.core",
 ]
 
 MIDDLEWARE = [
@@ -63,7 +65,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
