a2ffd026f51b3bd80d5cf772236b515e67793c97
Sungwoo Park_computer <sharkclass@naver.com>
2025-05-24 20:54:56 +0900

    feat: Add Price model in stock app (#10)

--- backend/apps/stock/models.py
+++ backend/apps/stock/models.py
@@ -38,3 +38,33 @@ class Stock(models.Model):
 
         def __str__(self):
             return f"{self.name} ({self.ticker})"
+
+
+class Price(models.Model):
+    """
+    Model representing the price of a stock.
+    """
+
+    stock = models.ForeignKey(
+        Stock, verbose_name=_("주식"), on_delete=models.CASCADE, related_name="prices"
+    )
+    date = models.DateField(verbose_name=_("날짜"))
+    open_price = models.DecimalField(
+        verbose_name=_("시가"), max_digits=10, decimal_places=2
+    )
+    high_price = models.DecimalField(
+        verbose_name=_("고가"), max_digits=10, decimal_places=2
+    )
+    low_price = models.DecimalField(
+        verbose_name=_("저가"), max_digits=10, decimal_places=2
+    )
+    close_price = models.DecimalField(
+        verbose_name=_("수정 종가"), max_digits=10, decimal_places=2
+    )
+    volume = models.BigIntegerField(verbose_name=_("거래량"))
+
+    class Meta:
+        verbose_name = _("가격")
+        verbose_name_plural = _("가격")
+        ordering = ["-date"]
+        unique_together = ("stock", "date")
