ccb0363452bc6de151ce0b43ce60d1c1437995ba
SWLaptop <swpark.biz@gmail.com>
2025-05-29 12:56:56 +0900

    feat: Add stock list and price list views (#23)

---
+++ backend/apps/stock/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import StockListView, PriceListView
+
+urlpatterns = [
+    path("stocks/", StockListView.as_view(), name="stock_list"),
+    path("prices/", PriceListView.as_view(), name="price_list"),
+]
--- backend/apps/stock/views.py
+++ backend/apps/stock/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Stock, Price
+
 
 # Create your views here.
+class StockListView(ListView):
+    model = Stock
+    template_name = "stock/stock_list.html"
+    context_object_name = "stocks"
+
+    def get_queryset(self):
+        return Stock.objects.all().order_by("name")
+
+
+class PriceListView(ListView):
+    model = Price
+    template_name = "stock/price_list.html"
+    context_object_name = "prices"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Price.objects.all().order_by("date")
