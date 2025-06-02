17df5a9b77bd413936b418cfbf132ed3491b984a
SWLaptop <swpark.biz@gmail.com>
2025-06-02 19:08:51 +0900

    feat: Add fetch complete notification with websocket (#21)

---
+++ backend/apps/stock/routing.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(
+        r"ws/price-update/(?P<user_id>\w+)/$",
+        consumers.PriceUpdateConsumer.as_asgi(),
+    )
+]
