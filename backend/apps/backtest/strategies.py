d82f20d274e462b24a2d1b1d849e49c5045a1733
SWLaptop <swpark.biz@gmail.com>
2025-07-08 13:56:07 +0900

    feat: Add backtest period (#51)

--- backend/apps/backtest/strategies.py
+++ backend/apps/backtest/strategies.py
@@ -4,14 +4,21 @@ import pandas as pd
 from apps.stock.models import Price
 
 
-def get_static_allocation_strategy(allocations: list[dict]) -> bt.Backtest:
+def get_static_allocation_strategy(
+    allocations: list[dict],
+    period: str = "monthly",
+    start_date: str = None,
+    end_date: str = None,
+) -> bt.Backtest:
 
     # 임의의 고정된 자산
     weights = {a["ticker"].ticker: a["weight"] for a in allocations}
     tickers = list(weights.keys())
 
     # 주가 데이터 조회
-    prices = Price.objects.get_adj_close_dataframe(tickers=tickers)
+    prices = Price.objects.get_adj_close_dataframe(
+        tickers=tickers, start_date=start_date, end_date=end_date
+    )
     prices.dropna(inplace=True)  # 결측치 제거
 
     # 전략 정의
