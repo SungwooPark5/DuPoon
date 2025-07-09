b231ff3e98e181e0712f673f749e364389f29706
SWLaptop <swpark.biz@gmail.com>
2025-07-09 14:34:40 +0900

    feat: Add more field to backtest form (#38)
    Added asset, rebalance frequency, slippage etc. Added javascript code so that user can add and remove assets.

---
+++ backend/static/js/backtest/backtest_page.js
@@ -0,0 +1,32 @@
+document
+  .getElementById("add-allocation")
+  .addEventListener("click", function () {
+    const allocationsDiv = document.getElementById("allocations");
+    const index = allocationsDiv.children.length;
+
+    const newAllocation = document.createElement("div");
+    newAllocation.className = "allocation row mb-2";
+
+    newAllocation.innerHTML = `
+        <div class="col-md-5">
+            <input name="allocations[${index}][ticker]" placeholder="티커" class="form-control mb-1" required="required">
+        </div>
+        <div class="col-md-5">
+            <input name="allocations[${index}][weight]" placeholder="비율 (%)" class="form-control mb-1" required="required">
+        </div>
+        <div class="col-md-2">
+            <button type="button" class="btn btn-outline-danger remove-allocation">삭제</button>
+        </div>
+    `;
+
+    allocationsDiv.appendChild(newAllocation);
+  });
+
+document
+  .getElementById("allocations")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("remove-allocation")) {
+      const allocationDiv = event.target.closest(".allocation");
+      allocationDiv.remove();
+    }
+  });
