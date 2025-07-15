5a9398a52ee41ef4360cf54398b131690f04470f
swpark <swpark.biz@gmail.com>
2025-07-15 13:45:00 +0900

    feat: Add strategy list page (#54)

--- backend/apps/strategy/urls.py
+++ backend/apps/strategy/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import StrategyView
+from .views import StrategyView, StrategyListView
 
 app_name = "strategy"
 
 urlpatterns =[
-    path("", StrategyView.as_view(), name="home")
+    path("", StrategyView.as_view(), name="home"),
+    path("list/", StrategyListView.as_view(), name="strategy_list"),
 ]
--- backend/apps/strategy/views.py
+++ backend/apps/strategy/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Strategy  
 
 # Create your views here.
 class StrategyView(TemplateView):
     template_name="strategy/home.html"
+    
+class StrategyListView(ListView):
+    model = Strategy
+    template_name = "strategy/strategy_list.html"
+    context_object_name = "strategies"
+
+    def get_queryset(self):
+        return Strategy.objects.all().order_by("name")
