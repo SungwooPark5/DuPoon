b515c2e8427c2a01d7600840945a9394143cf5a6
swpark <swpark.biz@gmail.com>
2025-07-29 15:26:18 +0900

    refactor: Refactor tests structure for stock app and add model manager test (#65)

---
+++ backend/apps/stock/tests/test_stock_manager.py
@@ -0,0 +1,14 @@
+import pytest
+import factory
+from datetime import date
+
+from apps.stock.models import Stock
+from apps.stock.factories import StockFactory
+
+
+@pytest.mark.django_db
+def test_get_distinct_tickers():
+    StockFactory(ticker="TST1")
+    StockFactory(ticker="TST2")
+    distinct_tickers = Stock.objects.get_distinct_tickers()
+    assert list(distinct_tickers) == ["TST1", "TST2"]
