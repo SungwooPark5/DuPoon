547a46de634072ad0cb659ec50d77254d28ad575
swpark <swpark.biz@gmail.com>
2025-08-25 15:18:17 +0900

    revert: "chore: Remove permission to fetch price (#76)"
    This reverts commit 3e447f505bc0e3295c8479b2db33fc6ab2729bda.

--- backend/apps/stock/views.py
+++ backend/apps/stock/views.py
@@ -46,6 +46,8 @@ class PriceFetchAPIView(APIView):
     API view to start fetching prices for all stocks.
     """
 
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         """
         Start fetching prices for all stocks.
